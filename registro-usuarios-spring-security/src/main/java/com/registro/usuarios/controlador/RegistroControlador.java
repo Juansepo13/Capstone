da7395e95719e59e32587f961b5cba31d79b4ca6
cristian <cristaker1927@hotmail.com>
2022-07-05 14:56:42 -0500

    update

--- registro-usuarios-spring-security/src/main/java/com/registro/usuarios/controlador/RegistroControlador.java
+++ registro-usuarios-spring-security/src/main/java/com/registro/usuarios/controlador/RegistroControlador.java
@@ -68,4 +68,84 @@ public class RegistroControlador {
 	public String contenidoBancoBogota() {
 		return "contenidobancobogota";
 	}
+	
+	@GetMapping("/contenidolibancolombia")
+	public String contenidoLIBancolombia() {
+		return "contenidolibancolombia";
+	}
+	
+	@GetMapping("/contenidolibbva")
+	public String contenidoLIBBVA() {
+		return "contenidolibbva";
+	}
+	
+	@GetMapping("/contenidolidavivienda")
+	public String contenidoLIDavivienda() {
+		return "contenidolidavivienda";
+	}
+	
+	@GetMapping("/contenidolibancobogota")
+	public String contenidoLIBancoBogota() {
+		return "contenidolibancobogota";
+	}
+	
+	@GetMapping("/contenidovbancolombia")
+	public String contenidoVBancolombia() {
+		return "contenidovbancolombia";
+	}
+	
+	@GetMapping("/contenidovbbva")
+	public String contenidoVBBVA() {
+		return "contenidovbbva";
+	}
+	
+	@GetMapping("/contenidovdavivienda")
+	public String contenidoVDavivienda() {
+		return "contenidovdavivienda";
+	}
+	
+	@GetMapping("/contenidovbancobogota")
+	public String contenidoVBancoBogota() {
+		return "contenidovbancobogota";
+	}
+	
+	@GetMapping("/contenidoccbancolombia")
+	public String contenidoCCBancolombia() {
+		return "contenidoccbancolombia";
+	}
+	
+	@GetMapping("/contenidoccbbva")
+	public String contenidoCCBBVA() {
+		return "contenidoccbbva";
+	}
+	
+	@GetMapping("/contenidoccdavivienda")
+	public String contenidoCCDavivienda() {
+		return "contenidoccdavivienda";
+	}
+	
+	@GetMapping("/contenidoccbancobogota")
+	public String contenidoCCBancoBogota() {
+		return "contenidoccbancobogota";
+	}
+	
+	@GetMapping("/adelantobancolombia")
+	public String AdelantoBancolombia() {
+		return "adelantobancolombia";
+	}
+	
+	@GetMapping("/adelantobbva")
+	public String AdelantoBBVA() {
+		return "adelantobbva";
+	}
+	
+	@GetMapping("/adelantodavivienda")
+	public String AdelantoDavivienda() {
+		return "adelantodavivienda";
+	}
+	
+	@GetMapping("/adelantobancobogota")
+	public String AdelantoBancoBogota() {
+		return "adelantobancobogota";
+	}
 }
